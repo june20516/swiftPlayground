bf0ccf29cd2c34e022dddea1304967ccc3e6e85f
Jun ho Lee <june20516@gmail.com>
2018-09-11 10:53:52 +0900

    구구단 2, 3단 구현
    직접 입력 후 print를 사용하여 출력

---
+++ gugu.playground/Contents.swift
@@ -0,0 +1,24 @@
+//: Playground - noun: a place where people can play
+
+import Cocoa
+
+print("2단")
+print("2 × 2 = \(2*2)")
+print("2 × 3 = \(2*3)")
+print("2 × 4 = \(2*4)")
+print("2 × 5 = \(2*5)")
+print("2 × 6 = \(2*6)")
+print("2 × 7 = \(2*7)")
+print("2 × 8 = \(2*8)")
+print("2 × 9 = \(2*9)")
+
+print("3단")
+print("3 × 2 = \(3*2)")
+print("3 × 3 = \(3*3)")
+print("3 × 4 = \(3*4)")
+print("3 × 5 = \(3*5)")
+print("3 × 6 = \(3*6)")
+print("3 × 7 = \(3*7)")
+print("3 × 8 = \(3*8)")
+print("3 × 9 = \(3*9)")
+
