c6f7321e4bb4b36326ae94684f403517bfd91a89
Jun ho Lee <june20516@gmail.com>
2018-09-11 16:39:03 +0900

    단위 변환기 .1
    command line tool 템플릿으로 생성
    데이터 형 변환 후 출력해보기

---
+++ UnitConverter/UnitConverter/main.swift
@@ -0,0 +1,22 @@
+//
+//  main.swift
+//  UnitConverter
+//
+//  Created by Jun ho Lee on 2018. 9. 11..
+//  Copyright © 2018년 Bran. All rights reserved.
+//
+
+import Foundation
+
+print("Hello, World!")
+
+
+let cm: Int = 120
+let m: Double = Double(cm) * 0.01
+print("\(m)m")
+
+
+let kg: Double = 0.27
+let g: Int = Int(kg * 1000)
+print("\(g)g")
+
